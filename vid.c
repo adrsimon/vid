ca5dd2bf151836d4a0373e96ce23fbf319a820f6
Adrien Simon <adrisim2001@gmail.com>
2024-11-28 21:53:05 +0100

    feat: load file

--- vid.c
+++ vid.c
@@ -1,8 +1,14 @@
+#include <sys/_types/_null.h>
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
@@ -23,9 +29,18 @@ enum editorKey {
   PAGE_DOWN
 };
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
+
+
 struct editorConfig {
   int cx, cy;
   int screenrows, screencols;
+  int numrows;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -142,6 +157,34 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {  
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+  }
+  free(line);
+  fclose(fp);
+}
+
 struct abuf {
   char *b;
   int len;
@@ -165,23 +208,29 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome), "Welcome in Vid Editor - Version %s", VID_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome), "Welcome in Vid Editor - Version %s", VID_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
       
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--) abAppend(ab, " ", 1);
-
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
+      int len = E.row[y].size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row[y].chars, len);
     }
-
+    
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
@@ -262,12 +311,19 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
+  E.row = NULL;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
+
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while(1) {
     editorRefreshScreen();
